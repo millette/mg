934d99d6b5e2e24de4aa2e8274778697007b3efa
Robin Millette <robin@millette.info>
2017-03-01 19:14:41 -0500

    Test posturl.

---
+++ w.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const yay = require('./lib/posturl')
+
+const content = `<p>Pif paf pow.
+So it goes. <img src="https://file.army/content/images/users/799Wf/av_1488342775.jpg?778"></p>
+<h2>More stuff</h2>
+Hmm, no tag?
+
+<ul>
+<li>List item!</li>
+</ul>
+<p>'nother image is <img src="http://robin.millette.info/api/aaa3/3c4e00304d305bafae810233171e9b5d/robin-mirroir-flop.jpg">
+<p>Ciao!</p>`
+
+yay.newImage(content)
+  .then((x) => {
+    console.log(x.length)
+    // console.log(Object.keys(x[0]))
+    console.log(x[0])
+    // console.log(Object.keys(x[1]))
+    console.log(x[1])
+  })
