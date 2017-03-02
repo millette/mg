b7e937bf75b0a0b96d7ac226a603f53c4bc9ec41
Robin Millette <robin@millette.info>
2017-03-02 18:30:55 -0500

    Routes...

--- plugins/img/index.js
+++ plugins/img/index.js
@@ -1,5 +1,31 @@
 'use strict'
 
+// npm
+const got = require('got')
+
+const imageRoute = function (request, reply) {
+  got(`http://localhost:5990/mesting/${request.params.shortid}/jpeg.jpg`, { encoding: null })
+    .then((x) => {
+      // console.log('H:', x.headers)
+      reply(x.body).type('image/jpeg').etag(x.headers.etag.slice(1, -1))
+    })
+  /*
+  reply(
+    got(`http://localhost:5990/mesting/${request.params.shortid}/jpeg.jpg`, { encoding: null })
+      .then((x) => x.body)
+  ).type('image/jpeg')
+  // ).header('content-type', 'image/jpeg')
+  */
+
+/*
+  console.log(request.params)
+  reply.redirect(
+`http://localhost:5990/mesting/${request.params.shortid}/jpeg.jpg`
+  )
+*/
+  // reply.view('an-img', { params: request.params })
+}
+
 exports.register = function (server, options, next) {
   server.route({
     path: '/img',
@@ -10,6 +36,20 @@ exports.register = function (server, options, next) {
       }
     }
   })
+
+  server.route({
+    path: '/i/{shortid}-o.jpg',
+    method: 'get',
+    handler: imageRoute
+/*
+    handler: {
+      view: {
+        template: 'an-img'
+      }
+    }
+*/
+  })
+
   next()
 }
 
