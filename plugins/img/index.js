a7648ecc1db9ac30a4751c03cedfe083bdd2124e
Robin Millette <robin@millette.info>
2017-03-03 16:05:24 -0500

    Handle missing images route (404).

--- plugins/img/index.js
+++ plugins/img/index.js
@@ -2,6 +2,7 @@
 
 // npm
 const got = require('got')
+// const boom = require('boom')
 
 const imageRoute = function (request, reply) {
   got(`http://localhost:5990/mesting/${request.params.shortid}/jpeg.jpg`, { encoding: null })
@@ -38,16 +39,20 @@ exports.register = function (server, options, next) {
   })
 
   server.route({
+    path: '/i/404.jpg',
+    method: 'get',
+    handler: function (request, reply) {
+      reply('Requested image could not be found.')
+        .type('image/jpeg')
+        .code(404)
+        .etag('404')
+    }
+  })
+
+  server.route({
     path: '/i/{shortid}-o.jpg',
     method: 'get',
     handler: imageRoute
-/*
-    handler: {
-      view: {
-        template: 'an-img'
-      }
-    }
-*/
   })
 
   next()
