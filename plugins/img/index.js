c17fcdcb11ccb796342b59a6620a073d300e5e44
Robin Millette <robin@millette.info>
2017-03-03 17:36:56 -0500

    Identify hardcoded jpeg lines.

--- plugins/img/index.js
+++ plugins/img/index.js
@@ -2,29 +2,15 @@
 
 // npm
 const got = require('got')
-// const boom = require('boom')
 
 const imageRoute = function (request, reply) {
+  // FIXME DB
   got(`http://localhost:5990/mesting/${request.params.shortid}/jpeg.jpg`, { encoding: null })
     .then((x) => {
-      // console.log('H:', x.headers)
+      // FIXME JPEG/PNG/GIF...
       reply(x.body).type('image/jpeg').etag(x.headers.etag.slice(1, -1))
     })
-  /*
-  reply(
-    got(`http://localhost:5990/mesting/${request.params.shortid}/jpeg.jpg`, { encoding: null })
-      .then((x) => x.body)
-  ).type('image/jpeg')
-  // ).header('content-type', 'image/jpeg')
-  */
-
-/*
-  console.log(request.params)
-  reply.redirect(
-`http://localhost:5990/mesting/${request.params.shortid}/jpeg.jpg`
-  )
-*/
-  // reply.view('an-img', { params: request.params })
+    .catch(reply)
 }
 
 exports.register = function (server, options, next) {
@@ -42,6 +28,7 @@ exports.register = function (server, options, next) {
     path: '/i/404.jpg',
     method: 'get',
     handler: function (request, reply) {
+      // FIXME JPEG/PNG/GIF...
       reply('Requested image could not be found.')
         .type('image/jpeg')
         .code(404)
@@ -50,6 +37,7 @@ exports.register = function (server, options, next) {
   })
 
   server.route({
+    // FIXME JPEG/PNG/GIF...
     path: '/i/{shortid}-o.jpg',
     method: 'get',
     handler: imageRoute
