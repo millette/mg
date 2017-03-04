44203b3862b66a353b96433b606d771d578fc4b0
Robin Millette <robin@millette.info>
2017-03-04 18:51:10 -0500

    Support multiple image types.

--- plugins/img/index.js
+++ plugins/img/index.js
@@ -1,44 +1,70 @@
 'use strict'
 
+// self
+const pkg = require('../../package.json')
+
+// core
+const path = require('path')
+
 // npm
 const got = require('got')
+const boom = require('boom')
+
+const image404 = function (request, reply) {
+  reply(boom.notFound('Requested image could not be found.'))
+}
 
 const imageRoute = function (request, reply) {
+  const ext = path.extname(request.path)
   // FIXME DB
-  got(`http://localhost:5990/mesting/${request.params.shortid}/jpeg.jpg`, { encoding: null })
+  got(`http://localhost:5990/mesting/${request.params.shortid}/jpeg${ext}`, { encoding: null })
     .then((x) => {
-      // FIXME JPEG/PNG/GIF...
-      reply(x.body).type('image/jpeg').etag(x.headers.etag.slice(1, -1))
+      reply(x.body).type(x.headers['content-type']).etag(x.headers.etag.slice(1, -1))
     })
-    .catch(reply)
+    .catch(() => image404(request, reply))
 }
 
 exports.register = function (server, options, next) {
   server.route({
-    path: '/img',
+    path: '/i/404.{ext}',
     method: 'get',
-    handler: {
-      view: {
-        template: 'an-img'
-      }
-    }
+    handler: image404
   })
 
+/*
   server.route({
     path: '/i/404.jpg',
     method: 'get',
-    handler: function (request, reply) {
-      // FIXME JPEG/PNG/GIF...
-      reply('Requested image could not be found.')
-        .type('image/jpeg')
-        .code(404)
-        .etag('404')
-    }
+    handler: image404
   })
 
   server.route({
-    // FIXME JPEG/PNG/GIF...
+    path: '/i/404.png',
+    method: 'get',
+    handler: image404
+  })
+
+  server.route({
+    path: '/i/404.gif',
+    method: 'get',
+    handler: image404
+  })
+*/
+
+  server.route({
     path: '/i/{shortid}-o.jpg',
+    method: 'get',
+    handler: imageRoute
+  })
+
+  server.route({
+    path: '/i/{shortid}-o.png',
+    method: 'get',
+    handler: imageRoute
+  })
+
+  server.route({
+    path: '/i/{shortid}-o.gif',
     method: 'get',
     handler: imageRoute
   })
@@ -48,5 +74,5 @@ exports.register = function (server, options, next) {
 
 exports.register.attributes = {
   name: 'img',
-  version: '0.0.0'
+  version: pkg.version
 }
