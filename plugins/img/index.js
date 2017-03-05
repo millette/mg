4dba90109185565269529a825f9e0da8a2611409
Robin Millette <robin@millette.info>
2017-03-04 19:32:38 -0500

    Yay, support multiple image types.

--- plugins/img/index.js
+++ plugins/img/index.js
@@ -5,10 +5,23 @@ const pkg = require('../../package.json')
 
 // core
 const path = require('path')
+const url = require('url')
 
 // npm
 const got = require('got')
 const boom = require('boom')
+
+const pluginOptions = {
+  username: process.env.CLOUDANT_USERNAME,
+  password: process.env.CLOUDANT_PASSWORD,
+  dbName: process.env.CLOUDANT_DATABASE
+}
+
+const dbUrl = (auth) => {
+  const urlObject = url.parse(`https://${pluginOptions.username}.cloudant.com/${pluginOptions.dbName}/`)
+  if (auth) { urlObject.auth = [pluginOptions.username, pluginOptions.password].join(':') }
+  return urlObject
+}
 
 const image404 = function (request, reply) {
   reply(boom.notFound('Requested image could not be found.'))
@@ -17,7 +30,14 @@ const image404 = function (request, reply) {
 const imageRoute = function (request, reply) {
   const ext = path.extname(request.path)
   // FIXME DB
-  got(`http://localhost:5990/mesting/${request.params.shortid}/jpeg${ext}`, { encoding: null })
+  const u = dbUrl(true)
+  const auth = u.auth
+  delete u.auth
+  u.pathname = [u.pathname, request.params.shortid, `jpeg${ext}`].join('/')
+  const u2 = url.format(u)
+  // const u = `http://localhost:5990/mesting/${request.params.shortid}/jpeg${ext}`
+  console.log('u2:', u2)
+  got(u2, { auth, encoding: null })
     .then((x) => {
       reply(x.body).type(x.headers['content-type']).etag(x.headers.etag.slice(1, -1))
     })
